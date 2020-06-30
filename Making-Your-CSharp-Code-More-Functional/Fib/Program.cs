9bf67c2faee34c45383fce3c8bc209d95a276422
Wenlin Huang <jackhwl@hotmail.com>
2020-06-29 21:45:51 -0400

    Fib version 0

---
+++ Making-Your-CSharp-Code-More-Functional/Fib/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fib
+{
+    class Program
+    {
+        static long NaiveFibonacci(int n) => n < 2 ? n : NaiveFibonacci(n-1) + NaiveFibonacci(n-2);
+
+        static void Demonstrate(Func<int, long> fibonacci)
+        {
+            for (int i=0;i<10;i++)
+                Console.WriteLine($"{i}\t{fibonacci(i)}");
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Demonstrate(NaiveFibonacci);
+            Console.ReadLine();
+        }
+    }
+}
