feba9851975b6951cc364aa9df4b65588afb494a
Wenlin Huang <jackhwl@hotmail.com>
2020-07-06 22:36:39 -0400

    more condition added

--- Making-Your-CSharp-Code-More-Object-Oriented/BranchingDemo/Account.cs
+++ Making-Your-CSharp-Code-More-Object-Oriented/BranchingDemo/Account.cs
@@ -9,14 +9,27 @@ namespace BranchingDemo
     class Account
     {
         public decimal Balance { get; private set;}
+
         private bool IsVarified { get; set;}
         private bool IsClosed { get; set; }
+        private bool IsFrozen { get; set; }
+
+        private Action OnUnfreeze { get; }
+        public Account(Action onUnfreeze)
+        {
+            this.OnUnfreeze = onUnfreeze;
+        }
 
         public void Deposit(decimal amount)
         {
             if (!this.IsClosed)
                 return;
             // Deposit money
+            if (this.IsFrozen)
+            {
+                this.IsFrozen = false;
+                this.OnUnfreeze();
+            }
             this.Balance += amount;
         }
 
@@ -26,6 +39,11 @@ namespace BranchingDemo
                 return;
             if (!this.IsClosed)
                 return;
+            if (this.IsFrozen)
+            {
+                this.IsFrozen = false;
+                this.OnUnfreeze();
+            }
             // Withdraw money
             this.Balance -= amount;
         }
@@ -38,6 +56,15 @@ namespace BranchingDemo
         public void Close()
         {
             this.IsClosed = true;
+        }
+
+        public void Frozen()
+        {
+            if (this.IsClosed)
+                return; // Account must not be closed
+            if (!this.IsVarified)
+                return; // Account must be verified
+            this.IsFrozen = true;
         }
     }
 }
