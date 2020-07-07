572d5fc406c28b8c77133d4dc934f39c374efb7a
Wenlin Huang <jackhwl@hotmail.com>
2020-07-06 21:39:23 -0400

    Branching Issue

---
+++ Making-Your-CSharp-Code-More-Object-Oriented/BranchingDemo/Account.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchingDemo
+{
+    class Account
+    {
+        public decimal Balance { get; private set;}
+        private bool IsVarified { get; set;}
+        private bool IsClosed { get; set; }
+
+        public void Deposit(decimal amount)
+        {
+            if (!this.IsClosed)
+                return;
+            // Deposit money
+            this.Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (!this.IsVarified)
+                return;
+            if (!this.IsClosed)
+                return;
+            // Withdraw money
+            this.Balance -= amount;
+        }
+
+        public void HolderVerified()
+        {
+            this.IsVarified = true;
+        }
+
+        public void Close()
+        {
+            this.IsClosed = true;
+        }
+    }
+}
