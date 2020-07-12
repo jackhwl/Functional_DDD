bc69e12e72b4b842602739448d1058cbfefc3a2e
Wenlin Huang <jackhwl@hotmail.com>
2020-07-12 19:49:12 -0400

    bad solution

---
+++ Making-Your-CSharp-Code-More-Object-Oriented/IteratorDemo/IPainter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorDemo
+{
+    public interface IPainter
+    {
+        bool IsAvailable { get; }
+        TimeSpan EstimateTimeToPaint(double sqMeters);
+        double EstimateCompensation(double sqMeters);
+    }
+}
---
+++ Making-Your-CSharp-Code-More-Object-Oriented/IteratorDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorDemo
+{
+    class Program
+    {
+        private static IPainter FindCheapestPainter(double sqMeters, IEnumerable<IPainter> painters)
+        {
+            double bestPrice = 0;
+            IPainter cheapest = null;
+
+            foreach (IPainter painter in painters)
+            {
+                if (painter.IsAvailable)
+                {
+                    double price = painter.EstimateCompensation(sqMeters);
+                    if (cheapest == null || price < bestPrice)
+                    {
+                        cheapest = painter;
+                    }
+                }
+            }
+
+            return cheapest;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
