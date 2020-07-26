381636f2b65dc226b359b6a97d02595d978fdb26
Wenlin Huang <jackhwl@hotmail.com>
2020-07-26 17:29:17 -0400

    Happy pass

---
+++ Making-Your-CSharp-Code-More-Object-Oriented/ValueObjectsDemo/MoneyAmount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueObjectsDemo
+{
+    class MoneyAmount
+    {
+        public decimal Amount { get; set; }
+        public string CurrencySymbol { get; set; }
+
+        public override string ToString() => $"{this.Amount} {this.CurrencySymbol}";
+    }
+}
---
+++ Making-Your-CSharp-Code-More-Object-Oriented/ValueObjectsDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueObjectsDemo
+{
+    class Program
+    {
+        static void Reserve(MoneyAmount cost)
+        {
+            Console.WriteLine("\nReserving an item that costs {0}.", cost);
+        }
+        static void Buy(MoneyAmount wallet, MoneyAmount cost)
+        {
+            bool enoughMoney = wallet.Amount >= cost.Amount;
+
+            Reserve(cost);
+
+            if (enoughMoney)
+                Console.WriteLine("You will pay {0} with your {1}.", cost, wallet);
+            else
+                Console.WriteLine("You cannot pay {0} with your {1}.", cost, wallet);
+        }
+
+        static void Main(string[] args)
+        {
+            Buy(new MoneyAmount() { Amount = 12, CurrencySymbol = "USD"},
+                new MoneyAmount() { Amount = 10, CurrencySymbol = "USD" });
+            Buy(new MoneyAmount() { Amount = 7, CurrencySymbol = "USD" },
+                new MoneyAmount() { Amount = 10, CurrencySymbol = "USD" });
+
+            Console.ReadLine();
+        }
+    }
+}
