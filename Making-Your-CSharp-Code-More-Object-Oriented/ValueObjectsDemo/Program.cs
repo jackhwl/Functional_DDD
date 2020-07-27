ed317d1b7eae0485309015f4c7344b887d9a55b6
Wenlin Huang <jackhwl@hotmail.com>
2020-07-26 22:29:43 -0400

    Fixing the Aliasing Bug

--- Making-Your-CSharp-Code-More-Object-Oriented/ValueObjectsDemo/Program.cs
+++ Making-Your-CSharp-Code-More-Object-Oriented/ValueObjectsDemo/Program.cs
@@ -9,20 +9,33 @@ namespace ValueObjectsDemo
     class Program
     {
         static bool IsHappyHour { get; set; }
-        static void Reserve(MoneyAmount cost)
+        static MoneyAmount Reserve(MoneyAmount cost)
         {
+            MoneyAmount newCost = cost;
             if (IsHappyHour)
-                cost.Amount *= .5M;
+            {
+                newCost = new MoneyAmount()
+                {
+                    Amount = cost.Amount * .5M,
+                    CurrencySymbol = cost.CurrencySymbol
+                };
+            }
             Console.WriteLine("\nReserving an item that costs {0}.", cost);
+
+            return newCost;
         }
         static void Buy(MoneyAmount wallet, MoneyAmount cost)
         {
             bool enoughMoney = wallet.Amount >= cost.Amount;
 
-            Reserve(cost);
+            MoneyAmount finalCost = Reserve(cost);
 
-            if (enoughMoney)
+            bool finalEnough = wallet.Amount >= finalCost.Amount;
+
+            if (enoughMoney && finalEnough)
                 Console.WriteLine("You will pay {0} with your {1}.", cost, wallet);
+            else if (finalEnough)
+                Console.WriteLine("This time, {0} will be enough to pay {1}.", wallet, finalCost);
             else
                 Console.WriteLine("You cannot pay {0} with your {1}.", cost, wallet);
         }
