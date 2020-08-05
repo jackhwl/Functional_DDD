5a865029ce373f5485e93dff29954dbcdfbf0389
Wenlin Huang <jackhwl@hotmail.com>
2020-08-04 21:07:39 -0400

    Initial Project

---
+++ Making-Your-CSharp-Code-More-Object-Oriented/NullObject/SoldArticle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullObject
+{
+    class SoldArticle
+    {
+        public Warranty MoneyBackGuarantee { get; }
+        public Warranty ExpressWarranty { get; }
+
+        public SoldArticle(Warranty moneyBack, Warranty express)
+        {
+            this.MoneyBackGuarantee = moneyBack;
+            this.ExpressWarranty = express;
+        }
+    }
+}
---
+++ Making-Your-CSharp-Code-More-Object-Oriented/NullObject/Warranty.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullObject
+{
+    class Warranty
+    {
+        private DateTime DateIssued { get; }
+        private TimeSpan Duration { get; }
+
+        public Warranty(DateTime dateIssued, TimeSpan duration)
+        {
+            this.DateIssued = dateIssued.Date;
+            this.Duration = TimeSpan.FromDays(duration.Days);
+        }
+
+        public bool IsValidOn(DateTime date) => 
+            date.Date >= this.DateIssued && date.Date < this.DateIssued + this.Duration;
+    }
+}
