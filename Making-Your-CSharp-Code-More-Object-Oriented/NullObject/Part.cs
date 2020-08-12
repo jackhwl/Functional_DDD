13f8e815354644a13f76dfce810b6a32e971568d
Wenlin Huang <jackhwl@hotmail.com>
2020-08-11 20:23:30 -0400

    0. Complicating the Requirements That Lead to Use of Nulls

---
+++ Making-Your-CSharp-Code-More-Object-Oriented/NullObject/Part.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullObject
+{
+    class Part
+    {
+        public DateTime InstallmentDate { get; }
+
+        public Part(DateTime installmentDate)
+        {
+            this.InstallmentDate = installmentDate;
+        }
+    }
+}
--- Making-Your-CSharp-Code-More-Object-Oriented/NullObject/SoldArticle.cs
+++ Making-Your-CSharp-Code-More-Object-Oriented/NullObject/SoldArticle.cs
@@ -10,8 +10,12 @@ namespace NullObject
     {
         public IWarranty MoneyBackGuarantee { get; private set; }
         public IWarranty ExpressWarranty { get; private set; }
+
         public IWarranty NotOperationalWarranty { get; }
- 
+        
+        private Part Circuitry { get; set; }
+        private IWarranty FailedCircuitryWarranty { get; set; }
+        private IWarranty CircuitryWarranty { get; set; }
         public SoldArticle(IWarranty moneyBack, IWarranty express)
         {
             if (moneyBack == null)
@@ -22,6 +26,7 @@ namespace NullObject
             this.MoneyBackGuarantee = moneyBack;
             this.ExpressWarranty = VoidWarranty.Instance;
             this.NotOperationalWarranty = express;
+            this.CircuitryWarranty = VoidWarranty.Instance;
         }
 
         public void VisibleDamage()
@@ -33,6 +38,14 @@ namespace NullObject
         {
             this.MoneyBackGuarantee = VoidWarranty.Instance;
             this.ExpressWarranty = this.NotOperationalWarranty;
+        }
+
+        private Part Circuitry { get; set; }
+        private IWarranty FailedCircuitryWarranty { get; set; }
+        public  void InstallCircuitry(Part circuitry, IWarranty extendedWarranty)
+        {
+            this.Circuitry = circuitry;
+            this.FailedCircuitryWarranty = extendedWarranty;
         }
     }
 }
