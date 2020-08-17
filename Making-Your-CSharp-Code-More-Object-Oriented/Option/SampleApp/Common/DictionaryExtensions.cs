08a405f874c8117adb9a5e960408aaabd408b91d
Wenlin Huang <jackhwl@hotmail.com>
2020-08-16 21:58:24 -0400

    Option solution

---
+++ Making-Your-CSharp-Code-More-Object-Oriented/Option/SampleApp/Common/DictionaryExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using CodingHelmet.Optional;
+
+namespace CodingHelmet.SampleApp.Common
+{
+    static class DictionaryExtensions
+    {
+        public static IOption<TValue> TryGetValue<TKey, TValue>(
+            this IDictionary<TKey, TValue> dictionary, TKey key)
+        {
+            TValue value;
+            if (!dictionary.TryGetValue(key, out value))
+                return Option.None<TValue>();
+            return Option.Some(value);
+        }
+    }
+}
---
+++ Making-Your-CSharp-Code-More-Object-Oriented/Option/SampleApp/Common/EnumerableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CodingHelmet.Optional;
+
+namespace CodingHelmet.SampleApp.Common
+{
+    static class EnumerableExtensions
+    {
+        public static IOption<T> AsOption<T>(this IEnumerable<T> sequence) =>
+            sequence
+                .Select(el => Option.Some(el))
+                .DefaultIfEmpty(Option.None<T>())
+                .Single();
+    }
+}
