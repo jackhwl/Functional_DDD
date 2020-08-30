93d9b3114cd13965274772d1646db3c077f52a8c
Wenlin Huang <jackhwl@hotmail.com>
2020-08-30 17:12:01 -0400

    2. Implementing Concrete Rules

---
+++ Making-Your-CSharp-Code-More-Object-Oriented/NullObject/NotOperationalRule.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullObject
+{
+    class NotOperationalRule : IWarrantyRules
+    {
+        private Action<Action> ClaimAction { get; }
+        private IWarrantyRules Next { get; }
+        private Action<Action> ClaimStrategy { get; set; }
+        public NotOperationalRule(Action<Action> claimAction, IWarrantyRules next)
+        {
+            this.ClaimAction = claimAction;
+            this.Next = next;
+        }
+        public void CircuitryOperational() { }
+        public void CircuitryFailed() { }
+        public void VisiblyDamaged() { }
+
+        public void NotOperational()
+        {
+            this.ClaimStrategy = (onValidClaim) => this.ClaimAction(onValidClaim);
+        }
+
+        public void Operational()
+        {
+            this.ClaimStrategy = (onValidClaim) => this.Next.Claim(onValidClaim);
+        }
+        public void Claim(Action onValidClaim) 
+        {
+            ClaimStrategy(onValidClaim);
+        }
+    }
+}
