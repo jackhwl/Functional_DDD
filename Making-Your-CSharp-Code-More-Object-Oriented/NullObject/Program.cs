01f1229417d81820945fb5050f4c387158ebc1ee
Wenlin Huang <jackhwl@hotmail.com>
2020-08-30 21:21:26 -0400

    5. Demonstrating the Flexibility of the Chain of Rules

--- Making-Your-CSharp-Code-More-Object-Oriented/NullObject/Program.cs
+++ Making-Your-CSharp-Code-More-Object-Oriented/NullObject/Program.cs
@@ -8,28 +8,35 @@ namespace NullObject
 {
     class Program
     {
-        static void ClaimWarranty(SoldArticle article)
-        {
-            DateTime now = DateTime.Now;
+        //static void ClaimWarranty(SoldArticle article)
+        //{
+        //    DateTime now = DateTime.Now;
 
-            article.MoneyBackGuarantee.Claim(now, () => Console.WriteLine("Offer money back."));
+        //    article.MoneyBackGuarantee.Claim(now, () => Console.WriteLine("Offer money back."));
 
-            article.ExpressWarranty.Claim(now, () => Console.WriteLine("Offer repair."));
-        }
+        //    article.ExpressWarranty.Claim(now, () => Console.WriteLine("Offer repair."));
+        //}
         static void Main(string[] args)
         {
-            DateTime sellingDate = new DateTime(2020, 8, 6);
-            TimeSpan moneyBackSpan = TimeSpan.FromDays(30);
-            TimeSpan warrantySpan = TimeSpan.FromDays(365);
+            //DateTime sellingDate = new DateTime(2020, 8, 6);
+            //TimeSpan moneyBackSpan = TimeSpan.FromDays(30);
+            //TimeSpan warrantySpan = TimeSpan.FromDays(365);
 
-            IWarranty moneyBack = new TimeLimitedWarranty(sellingDate, moneyBackSpan);
-            IWarranty warranty = new TimeLimitedWarranty(sellingDate, warrantySpan);
+            //IWarranty moneyBack = new TimeLimitedWarranty(sellingDate, moneyBackSpan);
+            //IWarranty warranty = new TimeLimitedWarranty(sellingDate, warrantySpan);
 
-             //IWarranty noMoneyBack = VoidWarranty.Instance;
-            IWarranty lifetimeWarranty = new LifetimeWarranty(sellingDate);
-            SoldArticle goods = new SoldArticle(VoidWarranty.Instance, lifetimeWarranty);
+            // //IWarranty noMoneyBack = VoidWarranty.Instance;
+            //IWarranty lifetimeWarranty = new LifetimeWarranty(sellingDate);
+            //SoldArticle goods = new SoldArticle(VoidWarranty.Instance, lifetimeWarranty);
 
-            ClaimWarranty(goods);
+            //ClaimWarranty(goods);
+
+            IWarranty moneyBackGuarantee = new TimeLimitedWarranty(DateTime.Today, TimeSpan.FromDays(7));
+            IWarranty expressWarranty = new TimeLimitedWarranty(DateTime.Today, TimeSpan.FromDays(365));
+            IWarranty circuitryWarranty = new LifetimeWarranty(DateTime.Today);
+
+            SoldArticle article = new SoldArticle(moneyBackGuarantee, expressWarranty, new DefaultRules());
+            article.InstallCircuitry(new Part(DateTime.Now), circuitryWarranty);
 
             Console.ReadLine();
         }
---
+++ Making-Your-CSharp-Code-More-Object-Oriented/NullObject/Rules/ChristmassRules.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullObject
+{
+    class ChristmassRules : IWarrantyRulesFactory
+    {
+        public IWarrantyRules Create(
+            Action<Action> claimMoneyBack,
+            Action<Action> claimNotOperationalWarranty,
+            Action<Action> claimCircuitryWarranty) =>
+                new ConditionalRule(IsAroundChristmas, claimMoneyBack,
+                   new DefaultRules().Create(claimMoneyBack, 
+                                             claimCircuitryWarranty,
+                                             claimCircuitryWarranty));
+
+        private static bool IsAroundChristmas() => DateTime.Today.Month == 12;
+    }
+}
